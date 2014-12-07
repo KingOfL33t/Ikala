13af9721df2a95e553c1417bb0ed41129125620f
KingOfL33t <KingOfL33tGaming@gmail.com>
2014-12-06 19:43:35 -0600

    Updated rng to seed based on a SecureRandom value for increased security.

--- src/com/ikalagaming/packages/rng/RngPackageMain.java
+++ src/com/ikalagaming/packages/rng/RngPackageMain.java
@@ -1,6 +1,8 @@
 
 package com.ikalagaming.packages.rng;
 
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -137,30 +139,11 @@ public class RngPackageMain implements Package, Listener{
 
 	@Override
 	public void onEnable() {
-		// get the current time of the system
-		long time1 = System.nanoTime();
-		/*
-		 * construct a random number of integers based on the last 3 digits of
-		 * the system time this will take a different amount of time based on
-		 * what value the hash was.
-		 */
-		for (int i = 0; i <= time1 % 1000; ++i) {
-			@SuppressWarnings("unused")
-			// not supposed to be used, int x is supposed to be inside the loop
-			int x = i + 2;// its value is trashed
-		}
-		// get the current time again
-		long time2 = System.nanoTime();
-
-		long deltaTime = time2 - time1;
-
-		int timeAsInt;
-		// make sure the long time can fit into an integer
-		while (deltaTime > Integer.MAX_VALUE) {
-			deltaTime = deltaTime - Integer.MAX_VALUE;
-		}
-		timeAsInt = (int) deltaTime;
-		gen = new Generator(timeAsInt);
+		SecureRandom sec = new SecureRandom();
+		byte[] sbuf = sec.generateSeed(8);
+		ByteBuffer bb = ByteBuffer.wrap(sbuf);
+		long seed = bb.getLong();
+		gen = new Generator((int) seed);
 	}
 
 	@Override
